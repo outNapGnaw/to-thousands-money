e80de54df25651a247c6f547769f7977c658ce85
peyton <WPTWWJ@163.com>
2020-04-04 23:46:50 +0800

    update reg

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 export default function format(num) {
     const str = num + ''
-    const strSplit = str.includes('.')?str.match(/(\S*)\.(\S*)$/):[str,str,'']
-    const left = strSplit[1].replace(/([-+]?\d)(?=(\d{3})+(?!\d))/g,'$1,')
-    const right = strSplit[2]
-    return right === '' ? left : `${left}.${right}`
+    const reg = /([-+]?\d)(?=(\d{3})+(?!\d))/g
+    return str.replace(/^([-+]?\d+)((\.\d+)?)$/g, function(s,s1,s2){
+      return s1.replace(reg,'$1,') + s2
+    })
 }
